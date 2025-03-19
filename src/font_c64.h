74d5f835d6169091d0c55c86844819c7df443f2a
stefan.trippler <stefan.trippler@betterask.erni>
2025-03-19 23:34:08 +0100

    fonts with new format (*.h/*.cpp) added

---
+++ src/font_c64.h
@@ -0,0 +1,12 @@
+#ifndef FONT_C64_H
+#define FONT_C64_H
+
+#include "font.h"
+
+class FontC64 : public Font
+{
+public: 
+  FontC64();
+}; 
+
+#endif
---
+++ src/font_default.h
@@ -0,0 +1,12 @@
+#ifndef FONT_DEFAULT_H
+#define FONT_DEFAULT_H
+
+#include "font.h"
+
+class FontDefault : public Font
+{
+public: 
+  FontDefault();
+}; 
+
+#endif
